c81c1a36ffe9fb2208f1b3ca93455d6cb3eef5f2
SAGAR SOLANKI <118007645+Sagarsolanki97@users.noreply.github.com>
2023-04-28 12:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function threeSum(S, target) {
+  S.sort(function(a, b) {
+    return a - b;
+  });
+  var closestSum = Infinity;
+  for (var i = 0; i < S.length - 2; i++) {
+    var j = i + 1;
+    var k = S.length - 1;
+    while (j < k) {
+      var sum = S[i] + S[j] + S[k];
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      if (sum < target) {
+        j++;
+      } else {
+        k--;
+      }
+    }
+  }
+  return closestSum;
 }
-
-module.exports = threeSum;
